2372e02564b515e77ed44a7aba8fe4bf4b8e2282
cho <qwertyu5258@naver.com>
2017-10-11 19:24:03 +0900

    한글 수정

--- Socket(다중)/src/chat/client/ClientBackground.java
+++ Socket(다중)/src/chat/client/ClientBackground.java
@@ -1,58 +1,59 @@
 package chat.client;
-
+ 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-
+ 
 public class ClientBackground {
-
-	private Socket socket;
-	private DataInputStream in;
-	private DataOutputStream out;
-	private ClientGui gui;
-	private String msg;
-	private String nickName;
-
-	public final void setGui(ClientGui gui) {
-		this.gui = gui;
-	}
-
-	public void connet() {
-		try {
-			socket = new Socket("127.0.0.1", 7777);
-			System.out.println("서버 대기중...");
-			
-			out = new DataOutputStream(socket.getOutputStream());
-			in = new DataInputStream(socket.getInputStream());
-			
-			//?젒?냽?븯?옄留덉옄 ?땳?꽕?엫 ?쟾?넚?븯硫?. ?꽌踰꾧? ?씠嫄? ?땳?꽕?엫?쑝濡? ?씤?떇?쓣 ?븯怨좎꽌 留듭뿉 吏묒뼱?꽔寃좎??슂?
-			out.writeUTF(nickName); 
-			System.out.println("?겢?씪?씠?뼵?듃 : 硫붿떆吏? ?쟾?넚?셿猷?");
-			while(in!=null){
-				msg=in.readUTF();
-				gui.appendMsg(msg);				
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void main(String[] args) {
-		ClientBackground clientBackground = new ClientBackground();
-		clientBackground.connet();
-	}
-
-	public void sendMessage(String msg2) {
-		try {
-			out.writeUTF(msg2);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public void setNickname(String nickName) {
-		this.nickName = nickName;
-	}
-
+ 
+    private Socket socket;
+    private DataInputStream in;
+    private DataOutputStream out;
+    private ClientGui gui;
+    private String msg;
+    private String nickName;
+ 
+    public final void setGui(ClientGui gui) {
+        this.gui = gui;
+    }
+ 
+    public void connet() {
+        try {
+            socket = new Socket("127.0.0.1", 7777);
+            System.out.println("서버 연결됨.");
+             
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+             
+            //접속하자마자 닉네임 전송하면. 서버가 이걸 닉네임으로 인식을 하고서 맵에 집어넣겠지요?
+            out.writeUTF(nickName); 
+            System.out.println("클라이언트 : 메시지 전송완료");
+            while(in!=null){
+                msg=in.readUTF();
+                gui.appendMsg(msg);             
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public static void main(String[] args) {
+        ClientBackground clientBackground = new ClientBackground();
+        clientBackground.connet();
+    }
+ 
+    public void sendMessage(String msg2) {
+        try {
+            out.writeUTF(msg2);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+ 
+    public void setNickname(String nickName) {
+        this.nickName = nickName;
+    }
+ 
 }
+ 
--- Socket(다중)/src/chat/server/ServerBackground.java
+++ Socket(다중)/src/chat/server/ServerBackground.java
@@ -1,5 +1,5 @@
 package chat.server;
-
+ 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
@@ -9,89 +9,92 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-
+ 
 public class ServerBackground {
-
-	private ServerSocket serverSocket;
-	private Socket socket;
-	private ServerGui gui;
-	private String msg;
-
-	
-	private Map<String, DataOutputStream> clientsMap = new HashMap<String, DataOutputStream>();
-
-	public final void setGui(ServerGui gui) {
-		this.gui = gui;
-	}
-
-	public void setting() throws IOException {
-			Collections.synchronizedMap(clientsMap); // ?씠嫄? 援먰넻?젙由? ?빐以띾땲?떎^^
-			serverSocket = new ServerSocket(7777);
-			while (true) {
-			
-				System.out.println("?꽌踰? ??湲곗쨷...");
-				socket = serverSocket.accept(); // 癒쇱? ?꽌踰꾧? ?븷?씪?? 怨꾩냽 諛섎났?빐?꽌 ?궗?슜?옄瑜? 諛쏅뒗?떎.
-				System.out.println(socket.getInetAddress() + "?뿉?꽌 ?젒?냽?뻽?뒿?땲?떎.");
-				// ?뿬湲곗꽌 ?깉濡쒖슫 ?궗?슜?옄 ?벐?젅?뱶 ?겢?옒?뒪 ?깮?꽦?빐?꽌 ?냼耳볦젙蹂대?? ?꽔?뼱以섏빞寃좎짛?!
-				Receiver receiver = new Receiver(socket);
-				receiver.start();
-			}
-	}
-
-	public static void main(String[] args) throws IOException {
-		ServerBackground serverBackground = new ServerBackground();
-		serverBackground.setting();
-	}
-
-	// 留듭쓽?궡?슜(?겢?씪?씠?뼵?듃) ???옣怨? ?궘?젣
-	public void addClient(String nick, DataOutputStream out) throws IOException {
-		sendMessage(nick + "?떂?씠 ?젒?냽?븯?뀲?뒿?땲?떎.");
-		clientsMap.put(nick, out);
-	}
-
-	public void removeClient(String nick) {
-		sendMessage(nick + "?떂?씠 ?굹媛??뀲?뒿?땲?떎.");
-		clientsMap.remove(nick);
-	}
-
-	// 硫붿떆吏? ?궡?슜 ?쟾?뙆
-	public void sendMessage(String msg) {
-		Iterator<String> it = clientsMap.keySet().iterator();
-		String key = "";
-		while (it.hasNext()) {
-			key = it.next();
-			try {
-				clientsMap.get(key).writeUTF(msg);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// -----------------------------------------------------------------------------
-	class Receiver extends Thread {
-		private DataInputStream in;
-		private DataOutputStream out;
-		private String nick;
-
-		/** XXX 2. 由ъ떆踰꾧? ?븳?씪?? ?옄湲? ?샎?옄?꽌 ?꽕?듃?썙?겕 泥섎━ 怨꾩냽..?뱽湲?.. 泥섎━?빐二쇰뒗 寃?. */
-		public Receiver(Socket socket) throws IOException {
-			out = new DataOutputStream(socket.getOutputStream());
-			in = new DataInputStream(socket.getInputStream());
-			nick = in.readUTF();
-			addClient(nick, out);
-		}
-
-		public void run() {
-			try {// 怨꾩냽 ?뱽湲곕쭔!!
-				while (in != null) {
-					msg = in.readUTF();
-					sendMessage(msg);
-					gui.appendMsg(msg);
-				}
-			} catch (IOException e) {
-				removeClient(nick);
-			}
-		}
-	}
+ 
+    // 지금까지 한일. GUi연동시키면서 서버Gui에 메시지띄움.
+    // 다음 이슈. Gui 상에서 일단 1:1 채팅을 하고 싶다.
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private ServerGui gui;
+    private String msg;
+ 
+    /** XXX 03. 세번째 중요한것. 사용자들의 정보를 저장하는 맵입니다. */
+    private Map<String, DataOutputStream> clientsMap = new HashMap<String, DataOutputStream>();
+ 
+    public final void setGui(ServerGui gui) {
+        this.gui = gui;
+    }
+ 
+    public void setting() throws IOException {
+            Collections.synchronizedMap(clientsMap); // 이걸 교통정리 해줍니다^^
+            serverSocket = new ServerSocket(7777);
+            while (true) {
+                /** XXX 01. 첫번째. 서버가 할일 분담. 계속 접속받는것. */
+                System.out.println("서버 대기중...");
+                socket = serverSocket.accept(); // 먼저 서버가 할일은 계속 반복해서 사용자를 받는다.
+                System.out.println(socket.getInetAddress() + "에서 접속했습니다.");
+                // 여기서 새로운 사용자 쓰레드 클래스 생성해서 소켓정보를 넣어줘야겠죠?!
+                Receiver receiver = new Receiver(socket);
+                receiver.start();
+            }
+    }
+ 
+    public static void main(String[] args) throws IOException {
+        ServerBackground serverBackground = new ServerBackground();
+        serverBackground.setting();
+    }
+ 
+    // 맵의내용(클라이언트) 저장과 삭제
+    public void addClient(String nick, DataOutputStream out) throws IOException {
+        sendMessage(nick + "님이 접속하셨습니다.");
+        clientsMap.put(nick, out);
+    }
+ 
+    public void removeClient(String nick) {
+        sendMessage(nick + "님이 나가셨습니다.");
+        clientsMap.remove(nick);
+    }
+ 
+    // 메시지 내용 전파
+    public void sendMessage(String msg) {
+        Iterator<String> it = clientsMap.keySet().iterator();
+        String key = "";
+        while (it.hasNext()) {
+            key = it.next();
+            try {
+                clientsMap.get(key).writeUTF(msg);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+ 
+    // -----------------------------------------------------------------------------
+    class Receiver extends Thread {
+        private DataInputStream in;
+        private DataOutputStream out;
+        private String nick;
+ 
+        /** XXX 2. 리시버가 한일은 자기 혼자서 네트워크 처리 계속..듣기.. 처리해주는 것. */
+        public Receiver(Socket socket) throws IOException {
+            out = new DataOutputStream(socket.getOutputStream());
+            in = new DataInputStream(socket.getInputStream());
+            nick = in.readUTF();
+            addClient(nick, out);
+        }
+ 
+        public void run() {
+            try {// 계속 듣기만!!
+                while (in != null) {
+                    msg = in.readUTF();
+                    sendMessage(msg);
+                    gui.appendMsg(msg);
+                }
+            } catch (IOException e) {
+                // 사용접속종료시 여기서 에러 발생. 그럼나간거에요.. 여기서 리무브 클라이언트 처리 해줍니다.
+                removeClient(nick);
+            }
+        }
+    }
 }
